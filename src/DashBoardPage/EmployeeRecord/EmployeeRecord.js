e9c829700eb2f1dbbb2d74bd0cd87f378ad504dc
Ayushi <ayushi17.sijaria@gmail.com>
2021-07-16 08:38:27 +0530

    test commit

--- src/DashBoardPage/EmployeeRecord/EmployeeRecord.js
+++ src/DashBoardPage/EmployeeRecord/EmployeeRecord.js
@@ -4,7 +4,8 @@ import classes from './EmployeeRecord.module.css'
 const EmployeeRecord = (props) => {
      return (
           <div className={classes.Employee}>
-               <p><span>Employee id:</span>{props.id}</p>
+               <p><span>Employee id:</span>
+               {props.id}</p>
                <p><span>Name:</span>{props.name}</p>
                <p><span>Age:</span> {props.age}</p>
                <p><span>Gender:</span> {props.gender}</p>
